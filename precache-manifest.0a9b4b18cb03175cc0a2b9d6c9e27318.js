8f5c3fad4cc0e8e0d690cc5e13c7fd791bf3f66e
Ko Zin <www.zinwine.mm@gmail.com>
2020-04-08 23:06:11 +0630

    Updates

---
+++ precache-manifest.0a9b4b18cb03175cc0a2b9d6c9e27318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b478e9625da9f8981ce147bcc827e4f",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "19c7507de26609bf5527",
+    "url": "/ContactManager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "19c7507de26609bf5527",
+    "url": "/ContactManager/static/js/2.459a6a03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.459a6a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3ad45b62b702c21c14",
+    "url": "/ContactManager/static/js/main.a206abf2.chunk.js"
+  },
+  {
+    "revision": "6d3a389a6b2314242afa",
+    "url": "/ContactManager/static/js/runtime-main.b60e2cff.js"
+  }
+]);
